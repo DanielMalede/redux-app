9260d2eab3c5d3398be5ae8e15fdc30bad2bbc50
DanielMalede <danielmalede@gmail.com>
2022-09-12 20:10:38 +0300

    .

--- src/store/action/user-action.js
+++ src/store/action/user-action.js
@@ -4,15 +4,12 @@ import { ADD_USER ,GET_BY_ID,UPDATE_USER,DELETE_USER} from "../types/users-types
 const addUserAction = () => {
     return { type: ADD_USER }
 }
-
 const getByIdAction = () => {
     return { type: GET_BY_ID, payload: userObj }
 }
-
 const updateUserAction = () => {
     return { type: UPDATE_USER, payload: userObj }
 }
-
 const deleteUserAction = () => {
     return { type: DELETE_USER, payload: userObj }
 }
