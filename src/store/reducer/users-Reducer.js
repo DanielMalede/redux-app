ba993558760dea0e7936e5a9cba78a322a1718b2
DanielMalede <danielmalede@gmail.com>
2022-09-12 20:08:48 +0300

    .

--- src/store/reducer/users-Reducer.js
+++ src/store/reducer/users-Reducer.js
@@ -1,6 +1,4 @@
 import { GET_BY_ID,ADD_USER,UPDATE_USER,DELETE_USER } from "../types/users-types";
-
-
 export default function usersReducer({users}, action) {
   switch (action.type) {
     case GET_BY_ID :
