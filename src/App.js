4a68067d5469c094083cbcc53a866928d773ec78
DanielMalede <danielmalede@gmail.com>
2022-09-13 19:12:39 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import Home from "./components/pages/Home/Home";
 function App() {
+  let a = {age:18};
+  let b = a;
+  console.log(b.age);
   return (
     <div className="App">
       <Home />
